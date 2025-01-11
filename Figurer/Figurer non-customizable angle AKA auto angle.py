94d3d0f33c3a651754ec4dd4398eadee97262e1f
Vr021210 <127669234+Vr021210@users.noreply.github.com>
2025-01-11 17:53:14 +0100

    Add files via upload

---
+++ Figurer/Figurer non-customizable angle AKA auto angle.py
@@ -0,0 +1,52 @@
+import customtkinter as ctk
+import turtle 
+turtle.speed("fastest")
+
+numbers_1to10 = ["1","2","3","4","5","6","7","8","9","10"]
+
+def draw():
+    kanterMengde = int(kanteterinput.get())
+    linjeLengde = int(linjeLengdeinput.get())
+    vinkel = 360 / kanterMengde
+    antall_figurer = int(antall_figurerinput.get())
+
+    def tegnFigur():
+        for i in range(kanterMengde):
+            turtle.forward(linjeLengde)
+            turtle.left(vinkel)
+
+    for i in range(antall_figurer):
+        tegnFigur()
+        turtle.left(360/antall_figurer)
+
+def erease():
+    turtle.clear()
+
+root = ctk.CTk()
+root.title("Turtle")
+root.geometry("800x300")
+
+
+canvas = ctk.CTkCanvas(root)
+canvas.pack()
+
+controller = ctk.CTkFrame(root)
+controller.pack()
+
+kanteterinput = ctk.CTkEntry(controller,placeholder_text="Kanter")
+kanteterinput.pack(side="left")
+
+linjeLengdeinput = ctk.CTkEntry(controller,placeholder_text="Linje lengde")
+linjeLengdeinput.pack(side="left")
+
+antall_figurerinput = ctk.CTkComboBox(controller,values=numbers_1to10)
+antall_figurerinput.pack(side="left")
+
+tegn = ctk.CTkButton(controller, text="Tegn", command=draw)
+tegn.pack(side="left")
+
+slett = ctk.CTkButton(controller, text="Slett", command=erease)
+slett.pack(side="left")
+
+
+root.mainloop()
